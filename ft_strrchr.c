3b0be4baa6d6f3c0116581ec6afd9c79ede3b94a
Jacket <>
2023-11-26 09:00:49 +0100

    Changed strrchr to not use a decrementing int, because pointers are funnier

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,30 +6,33 @@
 /*   By: jacket <jacket@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 12:42:56 by jacket            #+#    #+#             */
-/*   Updated: 2023/11/19 00:02:15 by jacket           ###   ########.fr       */
+/*   Updated: 2023/11/26 08:58:47 by jacket           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-// #include <stdlib.h>
-// #include <stdio.h>
-// #include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 char	*ft_strrchr(const char *s, int c)
 {
-	int		i;
+	int		lens;
 
-	i = strlen(s) - 1;
-	while (i--)
+	lens = strlen(s);
+	s += lens;
+	while (s-- && *s >= lens)
 	{
-		if (s[i] == c)
-			return ((char *) &s[i]);
+		if (*s == c)
+			return ((char *) s);
 	}
 	return (NULL);
 }
 
-// int main(int ac, char **av)
-// {
-// 	printf("%s\n", strrchr(av[1], *av[2]));
-// 	printf("%s\n", ft_strrchr(av[1], *av[2]));
-// }
+int main()
+{
+	char arr[] = "xello.how.are.you";
+	int c = 'u';
+	printf("%s\n", strrchr(arr, c));
+	printf("%s\n", ft_strrchr(arr, c));
+}
